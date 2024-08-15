3a53852c45b23b39f8167d912ce44b1563ea081f
2021baba <89834942+2021baba@users.noreply.github.com>
2024-08-15 17:52:29 +0800

    Add files via upload

---
+++ test22 - dowhile.cpp
@@ -0,0 +1,20 @@
+//22 do while语句
+#include<stdio.h>
+int main()
+{
+	int i=1;
+	do
+	{
+		if(i==5)
+			//;//break跳出循环，continue死循环 
+		
+		printf("%d\n",i);
+		i++;
+		
+	}while(i<=10);
+
+	return 0;
+ } //do循环至少执行一次
+  
+
+
